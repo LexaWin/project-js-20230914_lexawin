7c3731a1b826881599a26f04e91617e6920104b8
Aleksei Vinter <lexawin@gmail.com>
2023-10-22 23:57:56 +0300

    feat: add my ProductPage in project

--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -1,41 +1,114 @@
-import ProductForm from "../../../components/product-form";
+import SortableTable from '../../../components/sortable-table/index.js';
+import DoubleSlider from '../../../components/double-slider/index.js';
+import header from './bestsellers-header.js';
+// import fetchJson from "./utils/fetch-json.js";
+
+const BACKEND_URL = 'https://course-js.javascript.ru/';
 
 export default class Page {
   element;
-  subElements = {};
-  components = {};
+  subElements;
+
+  constructor() {}
+
+  createElement() {
+    const element = document.createElement('div');
+    element.classList.add('products-list');
+
+    return element;
+  }
+
+  createElementContentTemplate() {
+    return `
+      <div class="content__top-panel">
+        <h1 class="page-title">Products</h1>
+        <a href="#" class="button-primary">Add proudct</a>
+      </div>
+
+      <div class="content-box content-box_small">
+        <form class="form-inline">
+          <div class="form-group">
+            <label class="form-label">Filter by:</label>
+            <input
+              type="text"
+              data-elem="filterName"
+              class="form-control"
+              placeholder="Product name"
+            >
+          </div>
+
+          <div class="form-group" data-elem="sliderContainer">
+            <label class="form-label">Price:</label>
+            <div data-element="rangeSlider">
+              <!-- range-slider component -->
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="form-label">Status:</label>
+            <select class="form-control" data-elem="filterStatus">
+              <option value="" selected="">Any</option>
+              <option value="1">Active</option>
+              <option value="0">Inactive</option>
+            </select>
+          </div>
+        </form>
+      </div>
+
+      <div data-elem="productsContainer" class="products-list__container">
+        <div data-element="sortableTable">
+          <!-- sortable-table component -->
+        </div>
+      </div>
+    `;
+  }
+
+  getSubElements() {
+    const subElements = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+
+    for (const element of elements) {
+      const name = element.dataset.element;
+      subElements[name] = element;
+    }
+
+    return subElements;
+  }
+
+  createEventListeners() {}
+
+  removeEventListeners() {}
 
   async render() {
-    const element = document.createElement('div');
+    this.element = this.createElement();
+    this.element.innerHTML = this.createElementContentTemplate();
 
-    element.innerHTML = `
-      <div>
-        <h1>List page</h1>
-      </div>`;
+    this.subElements = this.getSubElements();
 
-    this.element = element.firstElementChild;
+    this.sortableTable = new SortableTable(header, {
+      url: 'api/rest/products',
+      isSortLocally: false,
+    });
+    this.subElements.sortableTable.append(this.sortableTable.element);
 
-    this.initComponents();
-    await this.renderComponents();
+    this.rangeSlider = new DoubleSlider({
+      min: 0,
+      max: 4000,
+      formatValue: (value) => '$' + value,
+    });
+    this.subElements.rangeSlider.append(this.rangeSlider.element);
+
+    this.createEventListeners();
 
     return this.element;
   }
 
-  initComponents() {
-    const productId = '101-planset-lenovo-yt3-x90l-64-gb-3g-lte-cernyj';
-
-    this.components.productFrom = new ProductForm(productId);
-  }
-
-  async renderComponents() {
-    const element = await this.components.productFrom.render();
-
-    this.element.append(element);
+  remove() {
+    this.element.remove();
   }
 
   destroy() {
-    for (const component of Object.values(this.components)) {
-      component.destroy();
-    }
+    this.removeEventListeners();
+    this.remove();
   }
 }
